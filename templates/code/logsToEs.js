5cc7d546ba1271a9217f4ec547054c00b0b3edc8
Thomas Trujillo <thomas.trujillo@taxdown.es>
2021-11-16 13:28:13 +0100

    Fix Error Response Parse

--- templates/code/logsToEs.js
+++ templates/code/logsToEs.js
@@ -189,28 +189,37 @@ function post(body, callback) {
             responseBody += chunk;
         });
         response.on('end', function() {
-            var info = JSON.parse(responseBody);
             var failedItems;
             var success;
-            
-            if (response.statusCode >= 200 && response.statusCode < 299) {
-                failedItems = info.items.filter(function(x) {
-                    return x.index.status >= 300;
-                });
+            if(isValidJson(responseBody)){
+                var info = JSON.parse(responseBody);
+                
+                if (response.statusCode >= 200 && response.statusCode < 299) {
+                    failedItems = info.items.filter(function(x) {
+                        return x.index.status >= 300;
+                    });
 
-                success = { 
-                    "attemptedItems": info.items.length,
-                    "successfulItems": info.items.length - failedItems.length,
-                    "failedItems": failedItems.length
+                    success = { 
+                        "attemptedItems": info.items.length,
+                        "successfulItems": info.items.length - failedItems.length,
+                        "failedItems": failedItems.length
+                    };
+                }
+
+                var error = response.statusCode !== 200 || info.errors === true ? {
+                    "statusCode": response.statusCode,
+                    "responseBody": responseBody
+                } : null;
+
+                callback(error, success, response.statusCode, failedItems);
+            }else{
+                var error = {
+                    "statusCode": response.statusCode,
+                    "responseBody": responseBody
                 };
+
+                callback(error, success, response.statusCode, failedItems);
             }
-
-            var error = response.statusCode !== 200 || info.errors === true ? {
-                "statusCode": response.statusCode,
-                "responseBody": responseBody
-            } : null;
-
-            callback(error, success, response.statusCode, failedItems);
         });
     }).on('error', function(e) {
         callback(e);
